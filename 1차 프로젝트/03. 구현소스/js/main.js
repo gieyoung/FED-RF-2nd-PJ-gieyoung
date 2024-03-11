01183cbaef75e50052810cb03e55170fe9d06dea
gieyoung <poohlee23@naver.com>
2024-03-11 17:45:09 +0900

    d

--- 1차 프로젝트/03. 구현소스/js/main.js
+++ 1차 프로젝트/03. 구현소스/js/main.js
@@ -1,32 +1,14 @@
-$(document).ready(function (){
+$(document).ready(function(){
 
+  // Swiper 슬라이더//
+  const progressCircle = document.querySelector(".autoplay-progress svg");
+  const progressContent = document.querySelector(".autoplay-progress span");
 
-  //硫붿씤�대�吏��щ씪�대뱶
-  $('.main_slider').bxSlider({
-      auto: true,
-      speed: 3000,
-      pause: 10000,
-      mode: 'fade',
-      autoControls: false,
-      arrows: false,
-      pager: false,
-      controls: false,
-      responsive: false,
-
-  });
-
- //�앹뾽�щ씪�대뱶
-  $('.bxslider').bxSlider({
-      auto: true,
-      speed: 1000,
-      pause: 5000,
-      mode: 'horizontal',
-      autoControls: false,
-      arrows: true,
-      pager: true,
-      controls: false,
-      responsive: false,
-
+  const swiperEl = document.querySelector("swiper-container");
+  swiperEl.addEventListener("autoplaytimeleft", (e) => {
+    const [swiper, time, progress] = e.detail;
+    progressCircle.style.setProperty("--progress", 1 - progress);
+    progressContent.textContent = `${Math.ceil(time / 1000)}s`;
   });
 
 
