238adaf4944f6e557b85e2ee1975d278c2bfd833
Tom B. Parker <tombap8@hotmail.com>
2024-03-11 18:16:46 +0900

    ss

--- 1차 프로젝트/03. 구현소스/js/main.js
+++ 1차 프로젝트/03. 구현소스/js/main.js
@@ -1,17 +1,30 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  // Swiper 슬라이더//
-  const progressCircle = document.querySelector(".autoplay-progress svg");
-  const progressContent = document.querySelector(".autoplay-progress span");
+//   // Swiper 슬라이더//
+//   const progressCircle = document.querySelector(".autoplay-progress svg");
+//   const progressContent = document.querySelector(".autoplay-progress span");
 
-  const swiperEl = document.querySelector("swiper-container");
-  swiperEl.addEventListener("autoplaytimeleft", (e) => {
-    const [swiper, time, progress] = e.detail;
-    progressCircle.style.setProperty("--progress", 1 - progress);
-    progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+//   const swiperEl = document.querySelector("swiper-container");
+//   swiperEl.addEventListener("autoplaytimeleft", (e) => {
+//     const [swiper, time, progress] = e.detail;
+//     progressCircle.style.setProperty("--progress", 1 - progress);
+//     progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+//   });
+
+
+
+
+// }); //��
+
+const headerWrap = document.querySelector(".header_wrap");
+const gnbList = document.querySelectorAll(".gnb-depth1");
+
+gnbList.forEach((ele)=>{
+  ele.addEventListener('mouseenter',()=>{
+    headerWrap.classList.add("on");
   });
+});
 
-
-
-
-}); //��
+headerWrap.addEventListener('mouseleave',()=>{  
+  headerWrap.classList.remove("on");
+});
