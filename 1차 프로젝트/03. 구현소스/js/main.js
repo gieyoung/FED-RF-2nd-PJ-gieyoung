0da809167ba1173efac1c2770f02cf8aba17f7b5
gieyoung <poohlee23@naver.com>
2024-03-13 18:17:50 +0900

    f

--- 1차 프로젝트/03. 구현소스/js/main.js
+++ 1차 프로젝트/03. 구현소스/js/main.js
@@ -1,22 +1,5 @@
-// $(document).ready(function(){
-
-//   // Swiper 슬라이더//
-//   const progressCircle = document.querySelector(".autoplay-progress svg");
-//   const progressContent = document.querySelector(".autoplay-progress span");
-
-//   const swiperEl = document.querySelector("swiper-container");
-//   swiperEl.addEventListener("autoplaytimeleft", (e) => {
-//     const [swiper, time, progress] = e.detail;
-//     progressCircle.style.setProperty("--progress", 1 - progress);
-//     progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-//   });
-
-
-
-
-// }); //��
-
-const headerWrap = document.querySelector(".header_wrap");
+// 헤더 마우스 오버시 2단메뉴 내려오기
+const headerWrap = document.querySelector(".nav_wrap");
 const gnbList = document.querySelectorAll(".gnb-depth1");
 
 gnbList.forEach((ele)=>{
