09a60e5a5a02c1115b966636d8127e671a978672
gieyoung <poohlee23@naver.com>
2024-03-29 18:15:31 +0900

    d

--- 1차 프로젝트/03. 구현소스/js/main.js
+++ 1차 프로젝트/03. 구현소스/js/main.js
@@ -72,13 +72,88 @@ play.onclick = function () {
 
 
 
+
+
+
+/****************햄버거 버튼****************/
+document.querySelector(".sitemap").onclick = function () {
+  document.querySelector(".hambtn").classList.toggle("on");
+};
+
+/****************다국어****************/
+document.querySelector(".btn-lang").onclick = function () {
+  document.querySelector(".lang-menu").classList.toggle("on");
+};
+
+/********************검색팝업********************/
+
+// 대상선정: 검색버튼
+const search = document.querySelector(".search");
+// 대상선정: 검색창
+const popSearch = document.querySelector("#top-area .pop-search");
+// 대상선정: 닫기버튼
+const delBtn = document.querySelector("#top-area .pop-search .del-btn");
+
+
+//검색아이콘 클릭시 검색창 뜨게
+search.onclick = () => {
+  popSearch.style.display = 'flex';
+
+};////////click///////////
+
+//닫기버튼 클릭시 검색창 닫게
+delBtn.onclick = () => {
+  popSearch.style.display = 'none';
+
+};////////click///////////
+
+/****************탭****************/
+
+// 대상선정: 탭버튼
+const tabBtn = document.querySelectorAll(".tab li a");
+// 대상선정: 탭리스트
+const tabList = document.querySelectorAll(".tab-list ul");
+//대상확인
+// console.log(tabBtn,tabList);
+
+//탭버튼 on넣고 빼기
+tabBtn.forEach((ele, idx) => {
+  ele.onclick = () => {
+    for (let x of tabBtn) {
+      x.classList.remove("on");
+    }/////////for of//////////
+    ele.classList.add("on");
+
+//현재 선택된 탭버튼과 순서가 일치하는 탭리스트에 on넣고 빼기
+    for (let x of tabList) {
+      x.classList.remove("on");
+    }/////////for of//////////
+    tabList[idx].classList.add("on");
+  };//////click//////////
+});//////////forEach////////////
+
+
+
+/********************탑버튼********************/
+
+const topBtn = document.querySelector(".top-btn");
+
+topBtn.onclick = () => {
+  window.scrollTo({top:0, behavior:'smooth'});
+};///////click///////////
+
+
+
+
+
+
 /****************제품소개 슬라이더(swiper2)****************/
 
 var swiper2 = new Swiper(".swiper-container2", {
   // Autoplay 활성화 및 옵션 설정
   loop: true,
   autoplay: {
-    delay: 1000, // 슬라이드 전환 시간 (밀리초)
+    delay: 5000, // 슬라이드 전환 시간 (밀리초)
     disableOnInteraction: false, // 사용자 상호 작용 후에도 자동 재생 유지
   },
   pagination: {
@@ -116,50 +191,19 @@ play2.onclick = function () {
 };
 
 
+/***********스크롤 위치 애니메이션*********/
 
-
-/****************햄버거 버튼****************/
-document.querySelector(".sitemap").onclick = function () {
-  document.querySelector(".hambtn").classList.toggle("on");
-};
-
-/****************다국어****************/
-document.querySelector(".btn_lang").onclick = function () {
-  document.querySelector(".lang-menu").classList.toggle("on");
-};
-
-/****************탭****************/
-
-// 대상선정: 탭버튼
-const tabBtn = document.querySelectorAll(".tab li a");
-// 대상선정: 탭리스트
-const tabList = document.querySelectorAll(".tab-list ul");
-//대상확인
-// console.log(tabBtn,tabList);
-
-//탭버튼 on넣고 빼기
-tabBtn.forEach((ele, idx) => {
-  ele.onclick = () => {
-    for (let x of tabBtn) {
-      x.classList.remove("on");
-    }/////////for of//////////
-    ele.classList.add("on");
-
-//현재 선택된 탭버튼과 순서가 일치하는 탭리스트에 on넣고 빼기
-    for (let x of tabList) {
-      x.classList.remove("on");
-    }/////////for of//////////
-    tabList[idx].classList.add("on");
-  };//////click//////////
-});//////////forEach////////////
+AOS.init(); 
 
 
 
-/********************탑버튼********************/
 
-const topBtn = document.querySelector(".top-btn");
+/*배너구역 배경 애니메이션*/ 
 
-topBtn.onclick = () => {
-  window.scrollTo({top:0, behavior:'smooth'});
-};///////click///////////
 
+  document.querySelectorAll('.bg-con')
+  .forEach(ele=>{
+    ele.onclick = ()=>{
+      ele.classList.toggle('on');
+    }; /// click ///
+  }); /// forEach ////
