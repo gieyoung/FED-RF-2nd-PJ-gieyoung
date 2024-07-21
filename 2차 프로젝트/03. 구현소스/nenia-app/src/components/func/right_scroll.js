b654e08d28f418b475997b70eb15cbb608aa7192
gieyoung <159221313+gieyoung@users.noreply.github.com>
2024-07-21 14:10:37 +0900

    d

--- 2차 프로젝트/03. 구현소스/nenia-app/src/components/func/right_scroll.js
+++ 2차 프로젝트/03. 구현소스/nenia-app/src/components/func/right_scroll.js
@@ -11,28 +11,26 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 // 스크롤트리거 플러그인을 불러오기
 gsap.registerPlugin(ScrollTrigger);
 
-
+var windowWidth = window.matchMedia("screen and (min-width:1200px)");
 
 export default function rightFn() {
-
-$(function () {
-  
-    var right_scroll = gsap.timeline({
-      scrollTrigger:{
-        trigger:'.horizon-scroll',
-        scrub:1,
-        // scrub은 스크롤 속도에 따라 애니메이션을 부드럽게 적용하도록 설정
-        start:'top 0',
-        end: 'bottom -150%',
-        pin:true,
-        // pin은 요소를 스크롤 시 고정시킴
-        toggleActions: 'play none none none',
-      }
+  // 디바이스 크기가 1200px 이상일 때 실행되도록 조건을 설정
+  if (windowWidth.matches) {
+    $(function () {
+      var right_scroll = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".horizon-scroll",
+          scrub: 1,
+          start: "top 0",
+          end: "bottom -150%",
+          pin: true,
+          toggleActions: "play none none none",
+        },
+      });
+      right_scroll.to(".horizon-scroll ul", {
+        xPercent: -160,
+        ease: "none",
+      });
     });
-    right_scroll.to('.horizon-scroll ul', {
-      xPercent: -160,
-      ease: "none",
-    });
-
-});
+  }
 }
